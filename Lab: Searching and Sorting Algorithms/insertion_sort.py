c71d7316427671714cea92e3ef78bc29129821e8
Mihaela Shemshirova <121877004+mihaelashemshirova@users.noreply.github.com>
2023-05-07 14:31:50 +0300

    Update insertion_sort.py

--- Lab: Searching and Sorting Algorithms/insertion_sort.py
+++ Lab: Searching and Sorting Algorithms/insertion_sort.py
@@ -4,5 +4,6 @@ for i in range(1, len(nums)):
     for j in range(i, 0, -1):
         if nums[j] < nums[j - 1]:
             nums[j], nums[j - 1] = nums[j - 1], nums[j]
-
+        else:
+            break
 print(*nums, sep=' ')
